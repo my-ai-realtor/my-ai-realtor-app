fb3c39207f054a6e64d6f833f1652f7c039218d0
Carlo Viloria <carloviloria808@gmail.com>
2024-10-29 17:10:15 -1000

    added transparent background, fixing eslint

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
       <Row className="text-center mt-4">
         <Col className="d-flex align-items-center justify-content-center">
           <Image 
-            src="https://cdn.discordapp.com/attachments/1278527340523159609/1300589784070094920/My_ai_realtor.png?ex=6722b5a0&is=67216420&hm=c4ad2f556e4af404832a9ef643d73e2b167442daf97f52531bf1aaf7170e15fc&" 
+            src="https://cdn.discordapp.com/attachments/1278527340523159609/1300590236551876761/My_ai_realtor_processed.png?ex=6722b60c&is=6721648c&hm=5359ccd562914bccf59211713097624054a7063b09abf6374c56244f57a81efa&" 
             alt="My AI Realtor logo" 
             id="Logo" 
             style={{ width: '200px', height: 'auto' }} 
@@ -18,7 +18,7 @@ const Footer = () => (
         <Col>
           <a href="/terms" className="footer-link-button mx-2">Terms of Service</a>
           <a href="/about" className="footer-link-button mx-2">About Us</a>
-          <a href="/faqs" className="footer-link-button mx-2">FAQs</a> 
+          <a href="/faqs" className="footer-link-button mx-2">FAQs</a>
         </Col>
       </Row>
       <Row className="text-center mt-3">
