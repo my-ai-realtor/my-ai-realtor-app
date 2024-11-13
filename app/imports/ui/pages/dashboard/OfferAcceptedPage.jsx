f9b82ab34cd8be2d0aaee6239b220f2d35a150bd
Carlo Viloria <carloviloria808@gmail.com>
2024-11-13 12:09:43 -1000

    update

---
+++ app/imports/ui/pages/dashboard/OfferAcceptedPage.jsx
@@ -0,0 +1,12 @@
+// /imports/ui/pages/dashboard/_Blank.jsx
+import React from 'react';
+
+const OfferAcceptedPage = () => (
+  <div>
+    <h2>Coming Soon</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit turpis et justo sagittis, nec lacinia metus gravida.</p>
+    <p>More details coming soon...</p>
+  </div>
+);
+
+export default OfferAcceptedPage;
