d8eb25d4110f45926c737e6c35935a31aa135af7
ChronoTrigger64 <sydneey@hawaii.edu>
2024-12-02 14:56:52 -1000

    made clear what is selection 4 page by adding a header that says it

--- app/imports/ui/pages/selection/SelectionFour.jsx
+++ app/imports/ui/pages/selection/SelectionFour.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SelectionFour = ({ setSelection }) => (
-  <button type="button" className="empty-button" onClick={() => setSelection(0)}>Next</button>
+  <>
+    <h1>selection 4 pg</h1>
+    <button
+      type="button"
+      className="empty-button"
+      onClick={() => setSelection(0)}
+    >
+      Next
+    </button>
+  </>
 );
 
 SelectionFour.propTypes = {
